cf6741532c5d7c7e73e2608e1fea81d9ff3373b0
CristinaLupu <cristinalupu975@gmail.com>
2020-11-16 15:49:55 +0200

    Now the snake is moving all four ways and I fixed another bug linked to the isOutOfBorder method

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ class SnakeGame {
   directionDictionary = {
     ArrowLeft: this.moveLeft,
     ArrowRight: this.moveRight,
-    ArrowUp: "up",
-    ArrowDown: "down",
+    ArrowUp: this.moveUp,
+    ArrowDown: this.moveDown,
   };
   speed = 15;
   intervalId;
+  axis;
 
   addEvents() {
     addEventListener("keydown", (e) => this.changeDirection(e.code));
   }
 
   changeDirection(newDirection) {
+    if (newDirection === "ArrowLeft" || newDirection === "ArrowRight") {
+      this.axis = "X";
+    } else if (newDirection === "ArrowUp" || newDirection === "ArrowDown") {
+      this.axis = "Y";
+    }
     if (this.directionDictionary[newDirection]) {
       this.directionDictionary[newDirection].apply(this);
     }
@@ -42,21 +48,45 @@ class SnakeGame {
       this.onSnakeMove();
     }, 150);
   }
+  moveDown() {
+    clearInterval(this.intervalId);
+    this.intervalId = setInterval(() => {
+      const top = window.getComputedStyle(this.snakeEl).top;
+      this.snakeEl.style.top = `${parseInt(top) + this.speed}px`;
+      this.onSnakeMove();
+    }, 150);
+  }
+  moveUp() {
+    clearInterval(this.intervalId);
+    this.intervalId = setInterval(() => {
+      const top = window.getComputedStyle(this.snakeEl).top;
+      this.snakeEl.style.top = `${parseInt(top) - this.speed}px`;
+      this.onSnakeMove();
+    }, 150);
+  }
 
   isOutOfBorder() {
     const left = parseInt(window.getComputedStyle(this.snakeEl).left);
     const top = parseInt(window.getComputedStyle(this.snakeEl).top);
 
-    if (left < 0 + this.snakeEl.clientWidth) {
-      return true;
-    } else if (top < 0) {
-      return true;
-    } else if (left >= this.boardEl.clientWidth - this.snakeEl.clientWidth) {
-      return true;
-    } else if (top >= this.boardEl.clientHeight - this.snakeEl.clientHeight) {
-      return true;
+    if (this.axis == "X") {
+      //* for left border
+      if (left < 0 + this.snakeEl.clientWidth) {
+        return true;
+        //* for right border
+      } else if (left >= this.boardEl.clientWidth - this.snakeEl.clientWidth) {
+        return true;
+      }
     }
-
+    if (this.axis == "Y") {
+      //* for top border
+      if (top < 0 + this.snakeEl.clientHeight) {
+        return true;
+        //* for bottom border
+      } else if (top >= this.boardEl.clientHeight - this.snakeEl.clientHeight) {
+        return true;
+      }
+    }
     return false;
   }
   constructor() {
